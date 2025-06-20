08d0cccb38d2315d9a1f97dff0986c55ef04621e
Mlie <emipa606@users.noreply.github.com>
2025-06-20 17:58:42 +0200

    Fixed harmony-transpiler

--- Source/Shortcuts/Dialog_Search_DoWindowContens.cs
+++ Source/Shortcuts/Dialog_Search_DoWindowContens.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using RimWorld;
 using UnityEngine;
@@ -25,19 +27,41 @@ public static class Dialog_Search_DoWindowContens
         return i == ShortcutsMain.currentSelection;
     }
 
-    private static IEnumerable<CodeInstruction> Transpiler(
-        IEnumerable<CodeInstruction> instructions)
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
         var matcher = new CodeMatcher(instructions)
             .MatchStartForward(Ldstr["DrawSearchResults"])
-            .MatchEndForward(CodeMatch.LoadsConstant(0), Stloc);
-        var iVar = matcher.Operand;
-        return matcher
+            .ThrowIfInvalid("Could not find 'DrawSearchResults'")
+            .MatchEndForward(CodeMatch.LoadsConstant(0), CodeMatch.IsStloc())
+            .ThrowIfInvalid("Could not find assignment to local int after 'DrawSearchResults'");
+
+        var iVar = matcher.Instruction.opcode switch
+        {
+            var op when op == OpCodes.Stloc_0 => 0,
+            var op when op == OpCodes.Stloc_1 => 1,
+            var op when op == OpCodes.Stloc_2 => 2,
+            var op when op == OpCodes.Stloc_3 => 3,
+            var op when op == OpCodes.Stloc_S || op == OpCodes.Stloc =>
+                matcher.Instruction.operand switch
+                {
+                    int val => val,
+                    LocalBuilder lb => lb.LocalIndex,
+                    _ => throw new InvalidOperationException(
+                        $"Unrecognized operand type: {matcher.Instruction.operand?.GetType()}")
+                },
+            _ => throw new InvalidOperationException($"Unexpected opcode: {matcher.Instruction.opcode}")
+        };
+
+
+        matcher = matcher
             .MatchStartForward(Call[mIsOver])
+            .ThrowIfInvalid("Could not find Mouse.IsOver call")
             .InsertAndAdvance(Ldloc[iVar])
-            .SetOperandAndAdvance(mMyIsOver)
-            .InstructionEnumeration();
+            .SetOperandAndAdvance(mMyIsOver);
+
+        return matcher.InstructionEnumeration();
     }
+
 
     private static void Prefix(SortedList<string, Thing> ___searchResults)
     {
